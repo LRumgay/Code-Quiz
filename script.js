b721c3074a600595681baf44048bb7a13fbc9db5
Liam Rumgay <Liamrumgay2@gmail.com>
2021-06-15 23:41:55 -0500

    added final css colors, and made questions in JS, ran into bug while trying to add questions.

--- script.js
+++ script.js
@@ -2,6 +2,50 @@ const startButton = document.querySelector("#startButton");
 var timeEl = document.querySelector("#timer-count");
 var secondsLeft = 60;
 
+
+const questions = [
+    {
+        question: "What does HTML stand for?",
+        answer: "Hypertext Markup Language",
+        options: ["Hoper Tony Manny Larry","Hypertext Markup Language", "Hypertalk Markup Language","Water"],
+
+    },
+
+    {
+        question: "Who founded JavaScript?",
+        answer: "Brendan Eich",
+        options:["Brendan Eich","Brendan Fraser","Nero Claudius Caesar Augustus Germanicus","Giorgio A. Tsoukalos"], 
+    },
+
+    {
+        question: "We can use Jquery on the quiz.",
+        answer: "False",
+        options: ["True","False"],
+    },
+
+    {
+        question: "What does DOM stand for?",
+        answer: "Document Object Model",
+        options: ["Dominic","Document Object Model","Dominion Objective Molecule","Don't Offend Me"],
+    },
+
+];
+
+
+function questionCard() {
+    var x = document.getElementById("question-card");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+
+
+
+
+//Timer initated after start button has been clicked//
 function setTime () {
     var timeInterval = setInterval(function() {
         secondsLeft--;
@@ -17,6 +61,11 @@ function startTime (){
     setTime();
 }
 
+startButton.addEventListener('click', () => {
+    setTime();
+    hidequestionCard();
+    showQuestion();
+});
 
 
 
@@ -34,7 +83,6 @@ function startTime (){
 
 
 
-startButton.addEventListener('click',startTime);
 
 
 
