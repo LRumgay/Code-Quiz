5976b1e123176af8ebd3860370f35c5553076cf2
Liam Rumgay <Liamrumgay2@gmail.com>
2021-06-16 22:57:37 -0500

    Added questions as well as functioning buttons

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const startButton = document.querySelector("#startButton");
 var timeEl = document.querySelector("#timer-count");
 var secondsLeft = 60;
+var QI = 0;
 
 
 const questions = [
@@ -32,39 +33,70 @@ const questions = [
 ];
 
 
-function questionCard() {
-    var x = document.getElementById("question-card");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
-  }
+function showQuestion() {
+    var qa = document.querySelector(".qa");
+    qa.removeAttribute("class", "hide");
+    changeQuestion();
+    setTime(); 
+}
 
-
-
+function changeQuestion() {
+    var questionEl = document.querySelector(".questions");
+    questionEl.textContent = questions[QI].question;
+    var answerBox = document.querySelector(".answer-box");
+    answerBox.innerHTML = ""
+    questions[QI].options.forEach(function (option){
+        console.log(option)
+        var button= document.createElement ("button"); 
+        button.textContent= option;
+        button.setAttribute("value",option)
+        button.onclick = evaluateAnswer;
+        answerBox.appendChild(button);
+    })
+}
+function evaluateAnswer(){
+console.log(this.value)
+//if statements to check right or wrong
+if (this.value !== questions[QI].answer){
+    console.log("wrong")
+} else {
+    console.log("right")
+}
+QI++;
+if(QI === questions.length) {
+    console.log("end game")
+} else {
+    changeQuestion()
+}
+}
 
 
 //Timer initated after start button has been clicked//
 function setTime () {
+    // run remove attribute class=hide
     var timeInterval = setInterval(function() {
         secondsLeft--;
         timeEl.textContent = secondsLeft;
-
+        
         if(secondsLeft === 0) {
             clearInterval(timeInterval);
         }
     },1000);
 }
-
-function startTime (){
-    setTime();
+//tested//
+function hidequestionCard(){
+    var questionCard = document.getElementById("question-card");
+    questionCard.setAttribute("class","hide")
+    showQuestion();
 }
 
-startButton.addEventListener('click', () => {
-    setTime();
+
+// function startTime (){
+//     setTime();
+// }
+
+startButton.addEventListener('click',function() {
     hidequestionCard();
-    showQuestion();
 });
 
 
